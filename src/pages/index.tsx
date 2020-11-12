24ae1341de39dd283695a47811059b57f7e1462f
bocai <woaichibocai_hxb@163.com>
2020-11-12 14:10:29 +0800

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React from 'react';
 import { Layout, Dropdown, Menu } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 const { Header, Footer, Content } = Layout;
 import { Row, Col, Button } from 'antd';
-import { openNotificationWithIcon, MessageWithAlink } from '../components/tools'
-import 'antd/dist/antd.css'; 
+import {
+  openNotificationWithIcon,
+  ethChainNameMap,
+  tronChainNameMap,
+} from '../components/tools';
+import 'antd/dist/antd.css';
 import Logo from '../static/suter_bridge_logo.png';
 import Home from '../components/home';
 import Form from '../components/form';
@@ -15,78 +19,88 @@ class SuterBridge extends React.Component {
     tronLinkInstalled: false,
     account: '',
     connectWalletTxt: 'Connect Wallet',
-    web3Browser: false, 
+    web3Browser: false,
     checkTronLinkCount: 0,
     formType: '',
     ethNetwork: '',
     tronNetwork: '',
-  }
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // this.checkWeb3Status = this.checkWeb3Status.bind(this)
-    this.checkMetaMaskStatus = this.checkMetaMaskStatus.bind(this)
-    this.checkTronLinkStatus = this.checkTronLinkStatus.bind(this)
-    this.setCurrentAccount = this.setCurrentAccount.bind(this)
-    this.dropDownMenu = this.dropDownMenu.bind(this)
-    this.connectMetaMask = this.connectMetaMask.bind(this)
-    this.connectTronLink = this.connectTronLink.bind(this)
-    this.checkEthNetworkType = this.checkEthNetworkType.bind(this)
-    this.checkTronNetworkType = this.checkTronNetworkType.bind(this)
+    this.checkMetaMaskStatus = this.checkMetaMaskStatus.bind(this);
+    this.checkTronLinkStatus = this.checkTronLinkStatus.bind(this);
+    this.setCurrentAccount = this.setCurrentAccount.bind(this);
+    this.dropDownMenu = this.dropDownMenu.bind(this);
+    this.connectMetaMask = this.connectMetaMask.bind(this);
+    this.connectTronLink = this.connectTronLink.bind(this);
+    this.checkEthNetworkType = this.checkEthNetworkType.bind(this);
+    this.checkTronNetworkType = this.checkTronNetworkType.bind(this);
   }
-	componentDidMount() {
+  componentDidMount() {
     // this.checkWeb3Status();
     this.checkMetaMaskStatus();
     this.interval = setInterval(this.checkTronLinkStatus, 1000);
   }
-  
+
   componentWillUnmount() {
-    clearInterval(this.interval)
+    clearInterval(this.interval);
   }
 
-  setCurrentAccount = (account: string, formType:string) => {
-    const connectWalletTxt = account.slice(0, 7) + '...' + account.slice(-5)
-    this.setState({ account: account, connectWalletTxt: connectWalletTxt, formType: formType})
-  }
+  setCurrentAccount = (account: string, formType: string) => {
+    const connectWalletTxt = account.slice(0, 7) + '...' + account.slice(-5);
+    this.setState({
+      account: account,
+      connectWalletTxt: connectWalletTxt,
+      formType: formType,
+    });
+  };
 
-  async connectMetaMask(){
+  async connectMetaMask() {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     const account = accounts[0];
     this.setCurrentAccount(account, 'Mint');
     this.clearExpiredTask(account);
   }
-  
-  async connectTronLink(){
-    const defaultAccount = await window.tronWeb.defaultAddress["base58"]
+
+  async connectTronLink() {
+    const defaultAccount = await window.tronWeb.defaultAddress['base58'];
     this.setCurrentAccount(defaultAccount, 'Revert');
-    this.clearRevertExpiredTask(defaultAccount)
+    this.clearRevertExpiredTask(defaultAccount);
   }
-  
+
   async checkTronLinkStatus() {
-    this.setState({checkTronLinkCount: this.state.checkTronLinkCount + 1})
-    if(typeof window.tronWeb !== 'undefined'){
+    this.setState({ checkTronLinkCount: this.state.checkTronLinkCount + 1 });
+    if (typeof window.tronWeb !== 'undefined') {
       console.log('TronLink is installed!');
-      this.setState({ tronLinkInstalled: true })
-      clearInterval(this.interval)
-      this.checkTronNetworkType()
-      this.tronChainChanged()
-    }else{
-      if(this.state.checkTronLinkCount >= 5){
-        clearInterval(this.interval)
-        const message = 'Suter Bridge must work with tronLink, please install tronLink'
-        openNotificationWithIcon('TronLink Is Not Install!', message, 'warning')
+      this.setState({ tronLinkInstalled: true });
+      clearInterval(this.interval);
+      this.checkTronNetworkType();
+      this.tronChainChanged();
+    } else {
+      if (this.state.checkTronLinkCount >= 5) {
+        clearInterval(this.interval);
+        const message =
+          'Suter Bridge must work with tronLink, please install tronLink';
+        openNotificationWithIcon(
+          'TronLink Is Not Install!',
+          message,
+          'warning',
+        );
       }
     }
   }
-  checkMetaMaskStatus(){
+  checkMetaMaskStatus() {
     if (typeof window.ethereum !== 'undefined') {
       console.log('MetaMask is installed!');
-      this.setState({ metamaskInstalled: true })
-      this.checkEthNetworkType()
-      this.ethChainChanged()
-    }else{
-      const message = 'Suter Bridge must work with metamask, please install metamask'
-      openNotificationWithIcon('MetaMask Is Not Install!', message, 'warning')
+      this.setState({ metamaskInstalled: true });
+      this.checkEthNetworkType();
+      this.ethChainChanged();
+    } else {
+      const message =
+        'Suter Bridge must work with metamask, please install metamask';
+      openNotificationWithIcon('MetaMask Is Not Install!', message, 'warning');
     }
   }
 
@@ -102,114 +116,167 @@ class SuterBridge extends React.Component {
   // }
 
   ethChainChanged() {
-    window.ethereum.on('chainChanged', (chainId) => {
-      openNotificationWithIcon('ETH Chain changed', 'Page will refresh after 2 seconds', 'warning', 4.5)
-      setTimeout(() => { window.location.reload() }, 2000);
-    })
+    window.ethereum.on('chainChanged', chainId => {
+      openNotificationWithIcon(
+        'ETH Chain changed',
+        'Page will refresh after 2 seconds',
+        'warning',
+        4.5,
+      );
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    });
   }
 
   tronChainChanged() {
-    window.addEventListener('message', function (e) {
-      if (e.data.message && e.data.message.action == "setNode") {
-          openNotificationWithIcon('TRON Chain changed', 'Page will refresh after 2 seconds', 'warning', 4.5)
-          setTimeout(() => { window.location.reload() }, 2000);
-        }
-    })
+    window.addEventListener('message', function(e) {
+      if (e.data.message && e.data.message.action == 'setNode') {
+        openNotificationWithIcon(
+          'TRON Chain changed',
+          'Page will refresh after 2 seconds',
+          'warning',
+          4.5,
+        );
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000);
+      }
+    });
   }
 
-  checkEthNetworkType(){
-    this.setState({ethNetwork: window.ethereum.chainId })
-   if(window.ethereum && window.ethereum.chainId != ETH_CHAIN_ID){
-      openNotificationWithIcon('ETH network error!', 'Please change metamask to ropsten network', 'warning', 4.5) 
-   }
-  }
-
-  checkTronNetworkType(){
-     this.setState({tronNetwork: window.tronWeb.currentProvider()["fullNode"]["host"].split('.')[1] })
-    if(window.tronWeb && window.tronWeb.currentProvider()["fullNode"]["host"].split('.')[1] != TRON_CHAIN_ID ){
-      openNotificationWithIcon('TRON network error!', 'Please change tronLink to shasta network', 'warning', 4.5)
+  checkEthNetworkType() {
+    this.setState({ ethNetwork: window.ethereum.chainId });
+    if (window.ethereum && window.ethereum.chainId != ETH_CHAIN_ID) {
+      openNotificationWithIcon(
+        'ETH network error!',
+        `Please change metamask to ${ethChainNameMap[ETH_CHAIN_ID]} network`,
+        'warning',
+        4.5,
+      );
     }
   }
 
-  clearExpiredTask(account){
+  checkTronNetworkType() {
+    const tronNetworkType = window.tronWeb
+      .currentProvider()
+      ['fullNode']['host'].split('.')[1];
+    this.setState({ tronNetwork: tronNetworkType });
+    if (tronNetworkType != TRON_CHAIN_ID) {
+      openNotificationWithIcon(
+        'TRON network error!',
+        `Please change tronLink to ${tronChainNameMap[TRON_CHAIN_ID]} network`,
+        'warning',
+        4.5,
+      );
+    }
+  }
+
+  clearExpiredTask(account) {
     // clear eth task
-    let taskQueue = (localStorage.getItem(`${account}Task`) || "").split(",")
+    let taskQueue = (localStorage.getItem(`${account}Task`) || '').split(',');
     taskQueue = taskQueue.filter(item => item);
-    let expiredKeys = {}
+    let expiredKeys = {};
     for (const key of taskQueue) {
-      let myTask = localStorage.getItem(key)
+      let myTask = localStorage.getItem(key);
       if (!myTask) {
-        continue
+        continue;
       }
-      const item = JSON.parse(myTask)
-      const now = new Date()
+      const item = JSON.parse(myTask);
+      const now = new Date();
       if (now.getTime() > item.expiry) {
-        localStorage.removeItem(key)
-        expiredKeys[key] = true
+        localStorage.removeItem(key);
+        expiredKeys[key] = true;
       }
     }
-    taskQueue = taskQueue.filter(item => !expiredKeys[item])
-    localStorage.setItem(`${account}Task`, taskQueue)
+    taskQueue = taskQueue.filter(item => !expiredKeys[item]);
+    localStorage.setItem(`${account}Task`, taskQueue);
   }
 
-  clearRevertExpiredTask(account){
+  clearRevertExpiredTask(account) {
     // clear eth task
-    let taskQueue = (localStorage.getItem(`${account}RevertTask`) || "").split(",")
+    let taskQueue = (localStorage.getItem(`${account}RevertTask`) || '').split(
+      ',',
+    );
     taskQueue = taskQueue.filter(item => item);
-    let expiredKeys = {}
+    let expiredKeys = {};
     for (const key of taskQueue) {
-      let myTask = localStorage.getItem(key)
+      let myTask = localStorage.getItem(key);
       if (!myTask) {
-        continue
+        continue;
       }
-      const item = JSON.parse(myTask)
-      const now = new Date()
+      const item = JSON.parse(myTask);
+      const now = new Date();
       if (now.getTime() > item.expiry) {
-        localStorage.removeItem(key)
-        expiredKeys[key] = true
+        localStorage.removeItem(key);
+        expiredKeys[key] = true;
       }
     }
-    taskQueue = taskQueue.filter(item => !expiredKeys[item])
-    localStorage.setItem(`${account}Task`, taskQueue)
+    taskQueue = taskQueue.filter(item => !expiredKeys[item]);
+    localStorage.setItem(`${account}Task`, taskQueue);
   }
 
   dropDownMenu = () => {
-    const { metamaskInstalled, tronLinkInstalled, ethNetwork, tronNetwork} = this.state
-    return(<Menu>
-      <Menu.Item key="1" onClick={() => this.connectMetaMask() } disabled={!metamaskInstalled || ethNetwork != ETH_CHAIN_ID }>
-        Bridge Ethereum Assets to Tron Assets
-      </Menu.Item>
-      <Menu.Item key="2" onClick={() => this.connectTronLink() } disabled={!tronLinkInstalled || tronNetwork != TRON_CHAIN_ID }>
-        Bridge Tron Assets to Ethereum Assets
-      </Menu.Item>
-    </Menu>)
-  }
-
-  render () {
-    const {connectWalletTxt, account, formType } = this.state
+    const {
+      metamaskInstalled,
+      tronLinkInstalled,
+      ethNetwork,
+      tronNetwork,
+    } = this.state;
     return (
-      <Layout className='suterBridge'>
+      <Menu>
+        <Menu.Item
+          key="1"
+          onClick={() => this.connectMetaMask()}
+          disabled={!metamaskInstalled || ethNetwork != ETH_CHAIN_ID}
+        >
+          Bridge Ethereum Assets to Tron Assets
+        </Menu.Item>
+        <Menu.Item
+          key="2"
+          onClick={() => this.connectTronLink()}
+          disabled={!tronLinkInstalled || tronNetwork != TRON_CHAIN_ID}
+        >
+          Bridge Tron Assets to Ethereum Assets
+        </Menu.Item>
+      </Menu>
+    );
+  };
+
+  render() {
+    const { connectWalletTxt, account, formType } = this.state;
+    return (
+      <Layout className="suterBridge">
         <Header>
           <Row>
-            <Col md={20} sm={12} >
-              <a href="/"><img src={ Logo } className='logo' /></a>
+            <Col md={20} sm={12}>
+              <a href="/">
+                <img src={Logo} className="logo" />
+              </a>
             </Col>
             <Col md={4} sm={12}>
-              { account !== '' ? <Button className="connectWalletBtn" >
-                 <div className='successDot'></div>
-                 { connectWalletTxt }
-              </Button> : 
-              <Dropdown overlay={this.dropDownMenu()}>
+              {account !== '' ? (
                 <Button className="connectWalletBtn">
-                  { connectWalletTxt }<DownOutlined />
+                  <div className="successDot"></div>
+                  {connectWalletTxt}
                 </Button>
-              </Dropdown>
-              }
+              ) : (
+                <Dropdown overlay={this.dropDownMenu()}>
+                  <Button className="connectWalletBtn">
+                    {connectWalletTxt}
+                    <DownOutlined />
+                  </Button>
+                </Dropdown>
+              )}
             </Col>
-         </Row>
+          </Row>
         </Header>
         <Content>
-         { account === '' ? <Home dropDownMenu={ this.dropDownMenu} /> : <Form account={account} formType={formType}/> }
+          {account === '' ? (
+            <Home dropDownMenu={this.dropDownMenu} />
+          ) : (
+            <Form account={account} formType={formType} />
+          )}
         </Content>
         <Footer></Footer>
       </Layout>
@@ -217,4 +284,4 @@ class SuterBridge extends React.Component {
   }
 }
 
-export default SuterBridge
+export default SuterBridge;
